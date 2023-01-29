8a93e23d62d024c24b08e58a7fd89fa6d73c9972
Ryan Garrett <RyanGar46@gmail.com>
2023-01-29 15:31:35 -0500

    Setup page layout

--- src/events.js
+++ src/events.js
@@ -1,8 +1,15 @@
 function postGameSetup(gameInfo) {
-  // Initialize game canvas
+  // Setup dynamic css
+  const style = document.createElement("style");
+  style.innerHTML = `:root {
+    aspect-ratio: ${gameInfo.aspect_ratio_width}/${gameInfo.aspect_ratio_height};
+  }
+  `;
+  document.body.appendChild(style);
+
+  // Fill game canvas placeholder
   $("canvas").removeAttr("style")
-    .attr("id", "game-canvas")
-    .css("aspect-ratio", `${gameInfo.aspect_ratio_width}/${gameInfo.aspect_ratio_height}`);
+    .attr("id", "game-canvas");
   const canvas = document.getElementById("game-canvas");
   const canvasPlaceholder = document.getElementById("game-canvas-placeholder");
   canvasPlaceholder.insertAdjacentElement("beforebegin", canvas);
